bd2bbd87258e9e5a3e3d57e7f18a4038878e6ca5
BenKoehler <ben.koehler@web.de>
2023-04-12 17:34:17 +0200

    update include dirs in bk.cmake

--- bk.cmake
+++ bk.cmake
@@ -3,4 +3,5 @@ set(BK_SDL_VERSION "7ec9a4385a0829ef2f7b101ec93f68065ddd2571") # SDL2 branch
 function(link_sdl2 target MOD)
     target_link_libraries(${target} ${MOD} ${PROJECT_SOURCE_DIR}/thirdparty/SDL2/lib/libSDL2-2.0.so.0.2700.0)
     target_include_directories(${target} SYSTEM ${MOD} ${PROJECT_SOURCE_DIR}/thirdparty/SDL2/include)
+    target_include_directories(${target} SYSTEM ${MOD} ${PROJECT_SOURCE_DIR}/thirdparty/SDL2/include/SDL2)
 endfunction()
